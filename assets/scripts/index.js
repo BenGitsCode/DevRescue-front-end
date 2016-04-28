506488c69e0d0a7c097546925dee315f8658a0d3
“Benjamski” <your_email@example.com>
2016-04-28 18:13:27 -0400

    Copied all of events.js to index.js
    commented out original

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,51 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// const getFormFields = require('../../../lib/get-form-fields.js');
+
+const authApi = require('./auth/api');
+const authUi = require('./auth/ui');
+
+
+const signCheck = () => {
+
+  $('#sign-up').on('submit', function (event) {
+    event.preventDefault();
+    let data = getFormFields(this);
+    authApi.signUp(authUi.success, authUi.failure, data);
+    console.log(data);
+  });
+
+  $('#sign-in').on('submit', function (event) {
+    let data = getFormFields(this);
+    event.preventDefault();
+    authApi.signIn(authUi.signInSuccess, authUi.failure, data);
+    console.log(data);
+  });
+
+  $('#sign-out').on('click', function (event) {
+    event.preventDefault();
+    authApi.signOut(authUi.signOutSuccess, authUi.failure);
+    console.log("signed out");
+  });
+
+  $('#change-password').on('submit', function (event) {
+    event.preventDefault();
+    let data = getFormFields(this);
+    authApi.changePass(authUi.success, authUi.failure, data);
+    console.log(data);
+  });
+};
+
+
+
+
+
+
+
+
+
+  module.exports = {
+   signCheck,
+  };
