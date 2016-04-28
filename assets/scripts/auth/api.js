0fd2f3aff5c37c2c7661c8e61ae929186adf65ff
“Benjamski” <your_email@example.com>
2016-04-28 15:42:30 -0400

    added files for user signin, out up and change pw functions   -copied and pasted the following from tic tac toe   - app-data.js   - api.js   - events.js   - ui.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const app = require('../app-data');
+
+
+const signUp = (success, failure, data) => {
+  $.ajax({
+    method: "POST",
+    url: app.api + '/sign-up',
+    data,
+
+  })
+  .done(success)
+  .fail(failure);
+
+};
+
+const signIn = (success, failure, data) => {
+  $.ajax({
+    method: "POST",
+    url: app.api + '/sign-in',
+    data,
+    dataProcessing: false,
+
+  }).done(success)
+    .fail(failure);
+};
+
+const signOut = (success, failure) => {
+  // if (!app.user) bad;
+  $.ajax({
+    method: "DELETE",
+    url: app.api + '/sign-out/' + app.user.id,
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const changePass = (success, failure, data) => {
+
+  $.ajax({
+    method: "PATCH",
+    url: app.api + '/change-password/' + app.user.id,
+    data,
+    // dataProcessing: false,
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut
+};
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const app = require('../app-data');
+// const newGame = require('./index.js');
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+  console.log(app);
+  console.log("Sign in successful");
+$('#new-game').click();
+};
+
+const signOutSuccess = () => {
+  app.user = null;
+  console.log(app);
+  console.log("You signed in bro. Sweet!");
+};
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+module.exports = {
+  failure,
+  success,
+  signOutSuccess,
+  signInSuccess,
+  app,
+};
